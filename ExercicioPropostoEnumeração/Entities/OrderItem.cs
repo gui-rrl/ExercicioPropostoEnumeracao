de44cc89e0d616f7fcf1e500b05a9cdd33283dd9
guilherme.rrl@gmail.com <guilherme.rrl@gmail.com>
2021-05-05 16:31:07 -0300

    Criado os atributos das Entidades, OrderItem, Client e Product

---
+++ ExercicioPropostoEnumeração/Entities/OrderItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioPropostoEnumeração.Entities
+{
+    class OrderItem
+    {
+
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+
+        public double Subtotal (double subtotal)
+        {
+            return subtotal += Quantity * Price;
+        }
+    }
+}
---
+++ ExercicioPropostoEnumeração/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ExercicioPropostoEnumeração.Entities;
+
+namespace ExercicioPropostoEnumeração
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+        }
+    }
+}
